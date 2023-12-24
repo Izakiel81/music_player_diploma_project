35315bd49f3301ea204d1200d51c6f435648748e
Makym <mgabin348@gmail.com>
2023-12-24 22:33:24 +0100

    initial_commit

---
+++ app/src/main/java/izakiel81/develop/music_player_diploma/MusicActivity.java
@@ -0,0 +1,14 @@
+package izakiel81.develop.music_player_diploma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music);
+    }
+}
