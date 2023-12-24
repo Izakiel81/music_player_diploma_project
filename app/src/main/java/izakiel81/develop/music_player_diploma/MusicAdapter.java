f28e5cf7bfc316facee287b76a2e5ff0e197145a
Makym <mgabin348@gmail.com>
2023-12-24 23:41:49 +0100

    MusicAdapter class added

---
+++ app/src/main/java/izakiel81/develop/music_player_diploma/MusicAdapter.java
@@ -0,0 +1,58 @@
+package izakiel81.develop.music_player_diploma;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicViewHolder> {
+
+    private ArrayList<String> list;
+    private Context mContext;
+
+    public MusicAdapter(ArrayList<String> list, Context mContext) {
+        this.list = list;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public MusicViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_layout, parent, false);
+
+        return new MusicViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MusicViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class MusicViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView textViewFileName;
+        private CardView cardView;
+
+        public MusicViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewFileName = itemView.findViewById(R.id.textViewFileName);
+            cardView = itemView.findViewById(R.id.cardViewItem);
+
+        }
+    }
+}
